462b807a233dab1b6bdeefa0f7b32269d886ec8f
nqui <imdangodaane@gmail.com>
2018-12-20 23:14:51 +0700

    Write things to graph.py, node.py as interactivepython

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from node import Node
 
 
 class Graph:
@@ -15,13 +16,50 @@ class Graph:
     directed graph(di-graph). The pair of the form (u, v) indicates that
     there is an edge from vertex u to vertex v.
     The edges may contain weight/value/cost.
-    
+
     Following two are the most commonly used representations of a graph.
     1. Adjacency Matrix
     2. Adjacency List
+
+    Vertex (Node)
+    Edge
+    -> Directed graph (one-way edges)
+
+    G = (V, E)
+    V = set of vertices
+    E = set of edges
+
+    Path (from one vertex to another vertex)
+    Cycle (from one vertex and return to that vertex)
+
+    When two vertices are connected by an edge, we say that they are "adjacent"
+
+    A matrix is not a very efficient way to store sparse data
+
+    Each vertex object in the graph maintains a list of the other vertices
+    that it is connected to.
     '''
     def __init__(self):
+        self.city_list = {}
+        self.num_of_city = 0
+
+    def add_city(self, city_name, latitude, longitude):
+        self.num_of_city += 1
+        new_city = Node(city_name, latitude, longitude)
+        self.city_list[city_name] = new_city
+        return new_city
+
+    def add_edge(self, first_vertex, second_vertex, weight=None):
         pass
+
+    def get_city(self, city_name):
+        if city_name in self.city_list:
+            return self.city_list[city_name]
+        else:
+            return None
+
+    def get_cities(self):
+        return self.city_list.keys()
 
     def find_shortest_path(self):
         # Object method using for find the shortest path and return it as a
--- node.py
+++ node.py
@@ -7,3 +7,20 @@ class Node:
         self.city_name = city_name
         self.latitude = latitude
         self.longitude = longitude
+        self.connected_to = {}
+
+    def add_neighbor(self, nbr, weight=0):
+        self.connected_to[nbr] = weight
+
+    def __str__(self):
+        return str(self.city_name) + 'connected to' + str([x.city_name
+                   for x in self.connected_to])
+
+    def get_connections(self):
+        return self.connected_to.keys()
+
+    def get_city_name(self):
+        return self.city_name
+
+    def get_weight(self, nbr):
+        return self.connected_to[nbr]
